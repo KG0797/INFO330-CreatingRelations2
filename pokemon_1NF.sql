014bbaedffacabf2e38db2257420c42bd7abc46e
KG0797 <121983886+KG0797@users.noreply.github.com>
2023-04-23 15:33:40 -0700

    Update pokemon_1NF.sql

--- pokemon_1NF.sql
+++ pokemon_1NF.sql
@@ -1,26 +1,26 @@
 CREATE TABLE pokemon_abilities(
-pokedex_number INT PRIMARY KEY,
+pokedex_number INT,
 name VARCHAR(255),
 ability VARCHAR(255),
-FOREIGN KEY (name) REFERENCES temp_p(name))
-
-ALTER TABLE imported_pokemon_data
-DROP COLUMN abilties;
+FOREIGN KEY (name) REFERENCES imported_pokemon_data(name),
+FOREIGN KEY (pokedex_number) REFERENCES imported_pokemon_data(pokedex_number));
 
 
-
-
-WITH RECURSIVE split (name, abilities, separate_abilities) AS (
-SELECT name, '' AS abilities, abilities||',' AS separate_abilities
+WITH RECURSIVE split (pokedex_number, name, abilities, separate_abilities) AS (
+SELECT pokedex_number, name, '' AS abilities, abilities||',' AS separate_abilities
 FROM imported_pokemon_data
 UNION ALL
-SELECT name,
+SELECT pokedex_number, name,
   substr(separate_abilities, 0, instr(separate_abilities, ',')) AS abilities,
   substr(separate_abilities, instr(separate_abilities, ',') +1) AS separate_abilities
 FROM split
 WHERE separate_abilities != ''
 )
-INSERT INTO pokemon_abilities(name, ability)
-SELECT name, TRIM(abilities) FROM split
+INSERT INTO pokemon_abilities(pokedex_number, name, ability)
+SELECT pokedex_number, name, abilities FROM split
 WHERE abilities != '';
+
+ALTER TABLE imported_pokemon_data
+DROP COLUMN abilties;
+
 
