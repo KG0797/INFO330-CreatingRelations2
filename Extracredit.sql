7975a5747b5c4a81fb53625033150834238e7caf
KG0797 <121983886+KG0797@users.noreply.github.com>
2023-04-23 19:31:07 -0700

    Create Extracredit.sql

---
+++ Extracredit.sql
@@ -0,0 +1,6 @@
+INSERT INTO imported_pokemon_data (name, type1, attack1, attack2, attack3)
+
+
+
+CREATE TABLE trainer(first_name VARCHAR(80), last_name VARCHAR(80));
+CREATE TABLE favorite_ptype(type VARCHAR(20),
