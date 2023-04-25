83398c0eb2a098f837ce77d2c3567bfb54651691
KG0797 <121983886+KG0797@users.noreply.github.com>
2023-04-25 14:37:39 -0700

    Update pokemon_3NF.sql

--- pokemon_3NF.sql
+++ pokemon_3NF.sql
@@ -1,5 +1,43 @@
-CREATE TABLE attack_defense(
-  name TEXT,
-  attack INT,
-  
-  type_name VARCHAR(20)
+CREATE TABLE if not EXISTS pokemon_info (
+name string,
+attack string,
+base_egg_steps string,
+base_happiness string,
+base_total string,
+capture_rate string,
+classfication string,
+defense string,
+experience_growth string,
+height_m string,
+hp string,
+percentage_male string,
+sp_attack string,
+sp_defense string,
+speed string,
+weight_kg string,
+generation string,
+is_legendary string,
+type1 string,
+type2 string
+);
+INSERT INTO pokemon_info
+SELECT name,
+attack,
+base_egg_steps,
+base_happiness,
+base_total,
+capture_rate,
+classfication,
+defense,
+experience_growth,
+height_m,
+hp,
+percentage_male,
+sp_attack,
+sp_defense,
+speed,
+weight_kg,
+generation,
+is_legendary,
+type1,
+type2 FROM imported_pokemon_data;
