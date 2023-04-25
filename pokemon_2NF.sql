7f14407e7aa5f2eb4eb42705ce5fcb995aef7dba
KG0797 <121983886+KG0797@users.noreply.github.com>
2023-04-24 18:54:33 -0700

    Update pokemon_2NF.sql

--- pokemon_2NF.sql
+++ pokemon_2NF.sql
@@ -1,44 +1,12 @@
-CREATE TABLE Pokemon( 
-  pokedex_number INT, 
-  name VARCHAR(55),
-  classification VARCHAR(50),
-  type1 VARCHAR(20),
-  type2 VARCHAR(20),
-  height_m DECIMAL(3,1),
-  weight_kg DECIMAL(4,1),
-  capture_rate INT,
-  base_egg_steps INT,
-  base_happiness INT,
-  base_total INT,
-  experience_growth INT,
-  generation INT,
-  is_legendary BOOLEAN
-);
-
-CREATE TABLE pokemon_stats(
-  pokedex_number INT,
-  hp,
-  attack,
-  defense,
-  sp_attack,
-  sp_defense,
-  speed
+CREATE TABLE type_against (
+  pokedex_number INT PRIMARY KEY,
+  name TEXT,
+  type1 TEXT, 
+  type2 TEXT
+  against_to INT,
+  FOREIGN KEY(type1) REFERENCES imported_pokemon_data(type1),
+  FOREIGN KEY(type2) REFERENCES imported_pokemon_data(type2)
 );
   
-CREATE TABLE against(
-  pokedex_number INT,
-  attacking_type_id INT,
-  against_type_id INT,
-  multiplier DECIMAL(2,1),
-  PRIMARY KEY (pokedex_number, attacking_type_id, against_type_id),
-  FOREIGN KEY(against_type_id) REFERENCES types(type1),
-  FOREIGN KEY(attacking_type_id) REFERENCES types(type2));
-  
-  
-
-CREATE TABLE type(
-  pokedex_number INT,
-  type TEXT,
-  FOREIGN KEY(pokedex_number) REFERENCES imported_pokemon_data(pokedex_number)
-  
-  
+INSERT INTO types(pokedex_number, name)
+SELECT DISTINCT type_name
