86de545728730026bf1c87f6aa311914dc3cbd1b
KG0797 <121983886+KG0797@users.noreply.github.com>
2023-04-25 16:32:19 -0700

    Update Extracredit.sql

--- Extracredit.sql
+++ Extracredit.sql
@@ -10,21 +10,47 @@ CREATE TABLE trainer(
 CREATE TABLE favorite_pokemon_types(
   id INT,
   type VARCHAR(70),
-  FOREIGH key(id) REFERENCES trainer(id)
+  FOREIGN KEY(id) REFERENCES trainer(id)
 );
 CREATE TABLE pokemon_team(
   id INT,
-  pokedex_number INT,
-  FOREIGH KEY(id) REFERENCES trainer(id),
-  FOREIGH KEY(pokedex_number) REFERENCES imported_pokemon_data(pokedex_number)
+  pokemon_member STRING,
+  FOREIGN KEY(id) REFERENCES trainer(id)
 );
         
-INSERT INTO trainer(first_name, last_name)
-  
-                     
-                     
-                    favorite_type1 TEXT, favorite_type2 TEXT,
-                    team_member TEXT,
-                    FOREIGN KEY(favorite_type1) REFERENCES imported
-                    );
-CREATE TABLE favorite_ptype(type VARCHAR(20),
+INSERT INTO trainer(id, first_name, last_name)
+VALUES (1, 'Ted', 'Neward'),
+       (2, 'Justin', 'Dong'),
+       (3, 'Katherine', 'Guo'),
+       (4, 'Jackie', 'Mao');
+
+INSERT INTO favorite_pokemon_types (id, type) 
+        VALUES(1, 'fighting'),
+              (1, 'dragon'),
+              (2, 'Water'),
+              (3, 'rock'),
+              (3, 'fire'),
+              (4, 'poison');
+INSERT INTO pokemon_team (id, pokemon1, pokemon2,  pokemon3,  pokemon4, pokemon5,  pokemon6,  pokemon7,  pokemon8,  pokemon9,  pokemon10)
+        VALUES (1, 'Bulbasaur', 'Ivysaur', 'Venusaur', 'Charmander', 'Charmeleon', NULL, NULL, NULL, NULL, NULL),
+        (2, 'Butterfree', 'Weedle', 'Kakuna', 'Beedrill', 'Pidgey', 'Pidgeotto', NULL, NULL, NULL, NULL),
+        (3, 'Pidgey', 'Pidgeotto', 'Pidgeot', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+        (4, 'Clefable', 'Vulpix', 'Ninetales', 'Jigglypuff', 'Wigglytuff', 'Zubat', 'Golbat', 'Oddish', 'Gloom', NULL);
+         
+                
+                
+ CREATE TABLE pokemon_team(
+  id INT,
+  pokemon1 STRING,
+  pokemon2 STRING,
+  pokemon3 STRING,
+  pokemon4 STRING,
+  pokemon5 STRING,
+  pokemon6 STRING,
+  pokemon7 STRING,
+  pokemon8 STRING,
+  pokemon9 STRING,
+  pokemon10 STRING,
+  FOREIGN KEY(id) REFERENCES trainer(id)
+);
+
