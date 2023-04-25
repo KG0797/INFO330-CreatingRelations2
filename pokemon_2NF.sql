43a52228f9bdb59567d62e148041e7a21dd29a35
KG0797 <121983886+KG0797@users.noreply.github.com>
2023-04-24 22:35:29 -0700

    Update pokemon_2NF.sql

--- pokemon_2NF.sql
+++ pokemon_2NF.sql
@@ -1,24 +1,30 @@
 CREATE TABLE type_against (
-  pokedex_number INT PRIMARY KEY,
-  name TEXT,
   type1 TEXT, 
-  type2 TEXT
-  against_bug INT,
-  against_dark INT,
-  against_dragon INT,
-  against_electric INT,
-  against_fairy INT,
-  against_fight INT,
-  against_fire INT,
-  against_flying INT,
-  against_ghost INT,
-  against_grass INT,
-  against_ground INT,
-  
+  type2 TEXT,
+  against_bug FLOAT,
+  against_dark FLOAT,
+  against_dragon FLOAT,
+  against_electric FLOAT,
+  against_fairy FLOAT,
+  against_fight FLOAT,
+  against_fire FLOAT,
+  against_flying FLOAT,
+  against_ghost FLOAT,
+  against_grass FLOAT,
+  against_ground FLOAT,
+  against_normal FLOAT,
+  against_poison FLOAT,
+  against_psychic FLOAT,
+  against_rock FLOAT,
+  against_steel FLOAT,
+  against_water FLOAT,
   FOREIGN KEY(type1) REFERENCES imported_pokemon_data(type1),
   FOREIGN KEY(type2) REFERENCES imported_pokemon_data(type2),
-  PRIMARY KEY(type1, type2)
+  PRIMARY KEY(type1, type2),
+  CONSTRAINT unique_type_combination UNIQUE(type1, type2)
 );
   
-INSERT INTO types(pokedex_number, name)
-SELECT DISTINCT type_name
+-- 
+INSERT INTO type_against (type1, type2, against_bug, against_dark, against_dragon, against_electric, against_fairy, against_fight, against_fire, against_flying, against_ghost, against_grass, against_ground, against_normal, against_poison, against_psychic, against_rock, against_steel, against_water)
+SELECT type1, type2, against_bug, against_dark, against_dragon, against_electric, against_fairy, against_fight, against_fire, against_flying, against_ghost, against_grass, against_ground, against_normal, against_poison, against_psychic, against_rock, against_steel, against_water
+FROM imported_pokemon_data;
