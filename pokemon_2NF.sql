44ced17642a6bb3975336f43e05b387b7a80cfd0
KG0797 <121983886+KG0797@users.noreply.github.com>
2023-04-24 19:08:43 -0700

    Update pokemon_2NF.sql

--- pokemon_2NF.sql
+++ pokemon_2NF.sql
@@ -3,9 +3,21 @@ CREATE TABLE type_against (
   name TEXT,
   type1 TEXT, 
   type2 TEXT
-  against_to INT,
+  against_bug INT,
+  against_dark INT,
+  against_dragon INT,
+  against_electric INT,
+  against_fairy INT,
+  against_fight INT,
+  against_fire INT,
+  against_flying INT,
+  against_ghost INT,
+  against_grass INT,
+  against_ground INT,
+  
   FOREIGN KEY(type1) REFERENCES imported_pokemon_data(type1),
-  FOREIGN KEY(type2) REFERENCES imported_pokemon_data(type2)
+  FOREIGN KEY(type2) REFERENCES imported_pokemon_data(type2),
+  FOREIGN KEY(
 );
   
 INSERT INTO types(pokedex_number, name)
