94832cc44147b0f3cc93e9b6208920a0da72f9b7
KG0797 <121983886+KG0797@users.noreply.github.com>
2023-04-25 14:56:43 -0700

    Update pokemon_1NF.sql

--- pokemon_1NF.sql
+++ pokemon_1NF.sql
@@ -1,31 +1,32 @@
-CREATE TABLE pokemon_abilities(
-pokedex_number INT,
-name STRING,
-ability STRING,
-seperate_abilities STRING,
-FOREIGN KEY (name) REFERENCES imported_pokemon_data(name),
-FOREIGN KEY (pokedex_number) REFERENCES imported_pokemon_data(pokedex_number));
+CREATE TABLE if not EXISTS ability_table (
+pokedex_number int,
+name string,
+ability string);
 
-
-WITH pokemon_abilities (pokedex_number, name, abilities, separate_abilities) AS (
-SELECT pokedex_number, name, '' AS abilities, abilities||',' AS separate_abilities
-FROM imported_pokemon_data
-UNION ALL
-SELECT pokedex_number, name,
-  substr(separate_abilities, 0, instr(separate_abilities, ',')) AS abilities,
-  substr(separate_abilities, instr(separate_abilities, ',') +1) AS separate_abilities
-FROM split
-WHERE separate_abilities is not null
+INSERT INTO ability_table
+SELECT pokedex_number,name,TRIM(TRIM(ability,'[' ),']') AS ability FROM ( WITH split_abilities AS (
+    SELECT 
+           pokedex_number,name,SUBSTR(abilities, 1, INSTR(abilities, ',') - 1) AS ability_1,
+           CASE WHEN INSTR(SUBSTR(abilities, INSTR(abilities, ',') + 1), ',') = 0 THEN
+                   SUBSTR(abilities, INSTR(abilities, ',') + 1)
+               ELSE
+                   SUBSTR(abilities, INSTR(abilities, ',') + 1, INSTR(SUBSTR(abilities, INSTR(abilities, ',') + 1), ',') - 1)
+           END AS ability_2,
+           CASE
+               WHEN INSTR(SUBSTR(abilities, INSTR(abilities, ',') + 1), ',') = 0 THEN
+                   NULL
+               ELSE
+                   SUBSTR(abilities, INSTR(abilities, ',') + 1 + INSTR(SUBSTR(abilities, INSTR(abilities, ',') + 1), ','))
+           END AS ability_3
+    FROM imported_pokemon_data
 )
-INSERT INTO pokemon_abilities(pokedex_number, name, ability)
-SELECT pokedex_number, name, abilities FROM split
-WHERE abilities is not null
-ORDER BY split.name;
+SELECT  pokedex_number,name,ability_1 AS ability FROM split_abilities
+UNION ALL
+SELECT  pokedex_number,name,ability_2 AS ability FROM split_abilities WHERE ability_2 IS NOT NULL
+UNION ALL
+SELECT pokedex_number,name,ability_3 AS ability FROM split_abilities WHERE ability_3 IS NOT NULL)
+WHERE ability>'';
 
-
-SELECT imported_pokemon_data.*, pokemon_abilities.ability 
-FROM imported_pokemon_data
-LEFT JOIN pokemon_abilities ON imported_pokemon_data.pokedex_number = pokemon_abilities.pokedex_number
 ALTER TABLE imported_pokemon_data
 DROP COLUMN abilties;
 
