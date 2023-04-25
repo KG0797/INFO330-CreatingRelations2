412134269f9f25919a0e8ced5c8dd636b6d261e3
KG0797 <121983886+KG0797@users.noreply.github.com>
2023-04-25 09:52:10 -0700

    Update pokemon_1NF.sql

--- pokemon_1NF.sql
+++ pokemon_1NF.sql
@@ -1,7 +1,7 @@
 CREATE TABLE pokemon_abilities(
 pokedex_number INT,
-name VARCHAR(255),
-ability VARCHAR(255),
+name STRING,
+ability STRING,
 FOREIGN KEY (name) REFERENCES imported_pokemon_data(name),
 FOREIGN KEY (pokedex_number) REFERENCES imported_pokemon_data(pokedex_number));
 
@@ -14,11 +14,11 @@ SELECT pokedex_number, name,
   substr(separate_abilities, 0, instr(separate_abilities, ',')) AS abilities,
   substr(separate_abilities, instr(separate_abilities, ',') +1) AS separate_abilities
 FROM split
-WHERE separate_abilities != ''
+WHERE separate_abilities is not null
 )
 INSERT INTO pokemon_abilities(pokedex_number, name, ability)
 SELECT pokedex_number, name, abilities FROM split
-WHERE abilities != ''
+WHERE abilities is not null
 ORDER BY split.name;
 
 
