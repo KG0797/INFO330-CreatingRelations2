46895879cce083fc484758ab77c28a43bdafca1a
KG0797 <121983886+KG0797@users.noreply.github.com>
2023-04-24 19:39:51 -0700

    Update Extracredit.sql

--- Extracredit.sql
+++ Extracredit.sql
@@ -1,6 +1,10 @@
-INSERT INTO imported_pokemon_data (name, type1, attack1, attack2, attack3)
+INSERT INTO imported_pokemon_data (pokedex_number, name, type1, attack1, attack2, attack3)
+VALUES (802, 'Huskichu', 'Mascot'
 
 
-
-CREATE TABLE trainer(first_name VARCHAR(80), last_name VARCHAR(80));
+CREATE TABLE trainer(first_name VARCHAR(80), last_name VARCHAR(80)
+                    favorite_type1 TEXT, favorite_type2 TEXT,
+                    team_member TEXT,
+                    FOREIGN KEY(favorite_type1) REFERENCES imported
+                    );
 CREATE TABLE favorite_ptype(type VARCHAR(20),
