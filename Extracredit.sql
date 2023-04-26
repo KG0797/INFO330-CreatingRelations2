4fedd7bfa28e0774d9e1ef35cbc22a08be75bebc
KG0797 <121983886+KG0797@users.noreply.github.com>
2023-04-25 23:01:09 -0700

    Update Extracredit.sql

--- Extracredit.sql
+++ Extracredit.sql
@@ -1,19 +1,14 @@
-INSERT INTO pokemon_info (pokedex_number, name, type1, attack, defense, hp, sp_attack, sp_defense, speed, generation)
-VALUES (802, 'Huskichu', 'Mascot', 34, 110, 130, 160, 110, 200, 2)
-VALUES (803, 'Cougarite', 'Mascot', 0, 5, 60, null, 10, 50, 1)
+INSERT INTO pokemon_info (pokedex_number, name, attack, base_egg_steps, base_happiness, base_total, capture_rate, classfication, defense, experience_growth, height_m, hp, percentage_male, sp_attack, sp_defense, speed, weight_kg, generation, is_legendary, pokemon_type)
+VALUES (802, 'Huskichu', 34, null, null, null, null, null, 110, null, null, 130, null, 160, 110, 200, null, 2, false, 'Mascot'),
+        (803, 'Cougarite', 0, null, null,null, null, null, 5, null, null, 60, null,  null, 10, 50, null, 1, false, 'Mascot');
 
+
+-- create a trainer table stores a couple of trainers and their pokemon team.
 CREATE TABLE trainer(
   id INT PRIMARY KEY,
   first_name VARCHAR(80),
-  last_name VARCHAR(80)
-);
-CREATE TABLE favorite_pokemon_types(
-  id INT,
-  type VARCHAR(70),
-  FOREIGN KEY(id) REFERENCES trainer(id)
-);
-CREATE TABLE pokemon_team(
-  id INT,
+  last_name VARCHAR(80),
+  favorite_pokemon_types STRING,
   pokemon1 STRING,
   pokemon2 STRING,
   pokemon3 STRING,
@@ -23,30 +18,11 @@ CREATE TABLE pokemon_team(
   pokemon7 STRING,
   pokemon8 STRING,
   pokemon9 STRING,
-  pokemon10 STRING,
-  FOREIGN KEY(id) REFERENCES trainer(id)
+  pokemon10 STRING
 );
 
-
-INSERT INTO trainer(id, first_name, last_name)
-VALUES (1, 'Ted', 'Neward'),
-       (2, 'Justin', 'Dong'),
-       (3, 'Katherine', 'Guo'),
-       (4, 'Jackie', 'Mao');
-
-INSERT INTO favorite_pokemon_types (id, type) 
-        VALUES(1, 'fighting'),
-              (1, 'dragon'),
-              (2, 'Water'),
-              (3, 'rock'),
-              (3, 'fire'),
-              (4, 'poison');
-INSERT INTO pokemon_team (id, pokemon1, pokemon2,  pokemon3,  pokemon4, pokemon5,  pokemon6,  pokemon7,  pokemon8,  pokemon9,  pokemon10)
-        VALUES (1, 'Bulbasaur', 'Ivysaur', 'Venusaur', 'Charmander', 'Charmeleon', NULL, NULL, NULL, NULL, NULL),
-        (2, 'Butterfree', 'Weedle', 'Kakuna', 'Beedrill', 'Pidgey', 'Pidgeotto', NULL, NULL, NULL, NULL),
-        (3, 'Pidgey', 'Pidgeotto', 'Pidgeot', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-        (4, 'Clefable', 'Vulpix', 'Ninetales', 'Jigglypuff', 'Wigglytuff', 'Zubat', 'Golbat', 'Oddish', 'Gloom', NULL);
-         
-                
-                
- 
+INSERT INTO trainer(id, first_name, last_name, favorite_pokemon_types, pokemon1, pokemon2,  pokemon3,  pokemon4, pokemon5,  pokemon6,  pokemon7,  pokemon8,  pokemon9,  pokemon10 )
+VALUES (1, 'Ted', 'Neward',  'fighting/dragon' ,'Bulbasaur', 'Ivysaur', 'Venusaur', 'Charmander', 'Charmeleon', NULL, NULL, NULL, NULL, NULL ),
+       (2, 'Justin', 'Dong', 'water', 'Butterfree', 'Weedle', 'Kakuna', 'Beedrill', 'Pidgey', 'Pidgeotto', NULL, NULL, NULL, NULL),
+       (3, 'Katherine', 'Guo', 'rock/fire', 'Pidgey', 'Pidgeotto', 'Pidgeot', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+       (4, 'Jackie', 'Mao', 'poison', 'Clefable', 'Vulpix', 'Ninetales', 'Jigglypuff', 'Wigglytuff', 'Zubat', 'Golbat', 'Oddish', 'Gloom', NULL);
